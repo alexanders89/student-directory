a69ebe999405e94b7eeef4ec9036b84838bf8995
Alex <alex.satur89@gmail.com>
2017-04-26 15:37:11 +0100

    Step 13

--- directory.rb
+++ directory.rb
@@ -6,14 +6,14 @@ def input_students
   # create an empty array
   @students = []
   #get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   until name.empty? do
   # add the student hash to the array
   @students << {name: name, cohort: :november}
   puts "Now we have #{@students.count} students"
   # get another name from the user
-  name = gets.chomp
+  name = STDIN.gets.chomp
   end
 end
 
@@ -55,7 +55,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -106,7 +106,7 @@ def save_students
   file.close
 end
 
-def load_students
+def load_students(filename = students.csv)
   puts ""
   puts "Loading file..."
   puts "*** File Loaded ***"
@@ -119,5 +119,17 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} does not exist"
+    exit
+  end
+end
 
+try_load_students
 interactive_menu
