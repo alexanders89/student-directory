85a2e0b72431151d053b1f91425072f76c27c6fe
Alex <alex.satur89@gmail.com>
2017-04-26 13:54:46 +0100

    Ex12 - if no input

---
+++ exercises/ex12.rb
@@ -0,0 +1,57 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  until name.empty? do
+  # add the student hash to the array
+  students << {name: name, cohort: :november}
+  puts "Now we have #{students.count} students"
+  # get another name from the user
+  name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header(students)
+  if students.count == 0
+    puts ""
+  else
+  puts "The students of my cohort at Makers Academy"
+  puts "-----------------"
+end
+end
+
+def print(students)
+  if students.count == 0
+    puts "*** You have not entered any data ***"
+  else
+  i = 0
+  num = students.count
+  until i >= num do
+    students.each_with_index do |student, index|
+      puts "#{index + 1}. #{student[:name].capitalize}, #{student[:cohort]} cohort"
+      i += 1
+    end
+  end
+end
+end
+
+
+def print_footer(students)
+  if students.count == 0
+    puts ""
+  else
+  puts "Overall, we have #{students.count} great students"
+end
+end
+
+
+students = input_students
+print_header(students)
+print(students)
+print_footer(students)
