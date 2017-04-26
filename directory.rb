559e0fcfb97f5d46996ca00708fd83680a51dc44
Alex <alex.satur89@gmail.com>
2017-04-26 15:16:38 +0100

    Directory

--- directory.rb
+++ directory.rb
@@ -1,33 +1,103 @@
-def print_header
-  puts "The students of Villains Academy"
-  puts "--------------"
-end
-
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer(names)
-  puts "Overall we have #{names.count} great students"
-end
-
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
+  # create an empty array
+  @students = []
+  #get the first name
   name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+  #while the name is not empty, repeat this code
+  until name.empty? do
+  # add the student hash to the array
+  @students << {name: name, cohort: :november}
+  puts "Now we have #{@students.count} students"
+  # get another name from the user
+  name = gets.chomp
   end
-  students
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_header
+  if @students.count == 0
+    puts ""
+  else
+  puts "The students of my cohort at Makers Academy"
+  puts "-----------------"
+end
+end
+
+def print_student_list
+  if @students.count == 0
+    puts "*** You have not entered any data ***"
+  else
+  i = 0
+  num = @students.count
+  until i >= num do
+    @students.each_with_index do |student, index|
+      puts "#{index + 1}. #{student[:name].capitalize}, #{student[:cohort]} cohort"
+      i += 1
+    end
+  end
+end
+end
+
+
+def print_footer
+  if @students.count == 0
+    puts ""
+  else
+  puts "Overall, we have #{@students.count} great students"
+  puts "-" * 80
+  puts ""
+end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+else
+  puts "-" * 80
+  puts "I dont know what you mean, please try again"
+  puts ""
+end
+end
+
+def save_students
+  file = File.open("students.csv" , 'w')
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
+interactive_menu
